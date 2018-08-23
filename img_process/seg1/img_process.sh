d9067417c64fdd40bffad36fd91868eb1ff11e25
caoyin <caoyin2011@163.com>
2018-08-23 15:14:23 +0800

    add img process on

--- img_process/seg1/img_process.sh
+++ img_process/seg1/img_process.sh
@@ -1,12 +1,13 @@
+
 # 搞一个i的变量
 i=1
 #for f in $(find ./seg1 -iname "*.*"); 
-for f in seg1/*
+for f in ./*
 do
+	#echo $i
 	news=img_$i
-	#echo $f
+	echo $f
 	((i++))
 	echo $news.pgm
-	convert $f -resize 320x240 $news.pgm
-	mv $news.pgm image/
+	#convert f -resize 320x240 $news.pgm
 done
